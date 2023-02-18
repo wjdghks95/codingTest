effecb61463cb46cf9a7b7f5cacd75a9df890e4c
wjdghks95 <wjdghks2799@Naver.com>
2023-02-18 17:57:05 +0900

    프로그래머스: 인덱스 바꾸기

--- src/lv_0/Test54.java
+++ src/lv_0/Test54.java
@@ -2,16 +2,17 @@ package lv_0;
 
 /**
  * 최대값 만들기(2)
+ *  정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
  */
 public class Test54 {
     public static void main(String[] args) {
         int[] numbers = {1, -2};
-        int solution = new Solution().solution(numbers);
+        int solution = new Solution54().solution(numbers);
         System.out.println("solution = " + solution);
     }
 }
 
-class Solution {
+class Solution54 {
     public int solution(int[] numbers) {
         int answer = Integer.MAX_VALUE * -1;
         for (int i = 0; i < numbers.length; i++) {
---
+++ src/lv_0/Test55.java
@@ -0,0 +1,44 @@
+package lv_0;
+
+/**
+ * 인덱스 바꾸기
+ *  문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+ */
+public class Test55 {
+    public static void main(String[] args) {
+        String my_string = "hello";
+        int num1 = 1;
+        int num2 = 2;
+        String solution = new Solution55().solution(my_string, num1, num2);
+        System.out.println("solution = " + solution);
+    }
+}
+
+class Solution55 {
+    public String solution(String my_string, int num1, int num2) {
+        char[] chars = my_string.toCharArray();
+        char temp = chars[num1];
+        chars[num1] = chars[num2];
+        chars[num2] = temp;
+
+        String answer = "";
+        for (char ch : chars) {
+            answer += ch;
+        }
+        return answer;
+    }
+}
+
+/**
+ * 모범답안
+ */
+/*
+class Solution {
+    public String solution(String myString, int num1, int num2) {
+        List<String> list = Arrays.stream(myString.split("")).collect(Collectors.toList());
+
+        Collections.swap(list, num1, num2);
+        return String.join("", list);
+    }
+}
+*/
