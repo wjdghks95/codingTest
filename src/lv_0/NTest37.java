445d8a97cb102634c05c8652e3defe075200d020
JEONGHWANLEE <wjdghks2799@mcocoa.com>
2023-05-02 13:31:27 +0900

    프로그래머스 -  수 조작하기 1

---
+++ src/lv_0/NTest37.java
@@ -0,0 +1,49 @@
+package lv_0;
+
+/**
+ * 수 조작하기 1
+ * 문제 설명
+ * 정수 n과 문자열 control이 주어집니다.
+ * control은 "w", "a", "s", "d"의 4개의 문자로 이루어져 있으며,
+ * control의 앞에서부터 순서대로 문자에 따라 n의 값을 바꿉니다.
+ *
+ * "w" : n이 1 커집니다.
+ * "s" : n이 1 작아집니다.
+ * "d" : n이 10 커집니다.
+ * "a" : n이 10 작아집니다.
+ * 위 규칙에 따라 n을 바꿨을 때 가장 마지막에 나오는 n의 값을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 제한사항
+ * -100,000 ≤ n ≤ 100,000
+ * 1 ≤ control의 길이 ≤ 100,000
+ * control은 알파벳 소문자 "w", "a", "s", "d"로 이루어진 문자열입니다.
+ */
+public class NTest37 {
+	public static void main(String[] args) {
+		int n = 0;
+		String control = "wsdawsdassw";
+		// result "-1"
+		int solution = new Solution().solution(n, control);
+		System.out.println("solution = " + solution);
+	}
+
+	static class Solution {
+		public int solution(int n, String control) {
+			int answer = 0;
+			char[] chars = control.toCharArray();
+			for (char c : chars) {
+				switch (c) {
+					case 'w' : n++;
+					break;
+					case 's' : n--;
+					break;
+					case 'd' : n += 10;
+					break;
+					case 'a' : n -= 10;
+					break;
+				}
+			}
+			return answer = n;
+		}
+	}
+}
