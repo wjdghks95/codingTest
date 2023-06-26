25973d8542bbb7a1113b79fe710dfd5f41912197
JEONGHWANLEE <wjdghks2799@naver.com>
2023-06-26 10:57:22 +0900

    프로그래머스 - 특수문자 출력하기

---
+++ src/lv_0/Test126.java
@@ -0,0 +1,16 @@
+package lv_0;
+
+/**
+ * 특수문자 출력하기
+ * 문제 설명
+ * 다음과 같이 출력하도록 코드를 작성해 주세요.
+ *
+ * 출력 예시
+ * !@#$%^&*(\'"<>?:;
+ */
+public class Test126 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
+
