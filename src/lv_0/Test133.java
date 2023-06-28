a862ae908bdf90c1d9fe50c621f3b61e8e6c67ba
JEONGHWANLEE <wjdghks2799@naver.com>
2023-06-28 11:44:22 +0900

    프로그래머스 - 삼각형의 완성조건 (2)

---
+++ src/lv_0/Test133.java
@@ -0,0 +1,57 @@
+package lv_0;
+
+/**
+ * 삼각형의 완성조건 (2)
+ * 문제 설명
+ * 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+ * 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+ * 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다.
+ * 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * sides의 원소는 자연수입니다.
+ * sides의 길이는 2입니다.
+ * 1 ≤ sides의 원소 ≤ 1,000
+ *
+ * 입출력 예
+ * sides	result
+ * [1, 2]	1
+ * [3, 6]	5
+ * [11, 7]	13
+ */
+public class Test133 {
+    public static void main(String[] args) {
+        int[] sides = {11, 7};
+        int solution = new Solution().solution(sides);
+        System.out.println("solution = " + solution);
+    }
+
+    static class Solution {
+        public int solution(int[] sides) {
+            int answer = 0;
+            int max = Math.max(sides[0], sides[1]);
+            int min = Math.min(sides[0], sides[1]);
+            for (int i = max - min + 1; i <= max; i++) {
+                answer++;
+            }
+            for (int i = max + 1; i < max + min; i++) {
+                answer++;
+            }
+
+            return answer;
+        }
+
+        /*
+         public int solution(int[] sides) {
+            int answer = 0;
+            int max = Math.max(sides[0], sides[1]);
+            int min = Math.min(sides[0], sides[1]);
+
+            answer += min * 2 - 1;
+    
+            return answer;
+        }
+         */
+    }
+}
+
