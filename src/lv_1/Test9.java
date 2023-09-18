0fe613481736b66d4a18d2cfd637bfa483b1e2cd
JEONGHWANLEE <wjdghks2799@naver.com>
2023-09-18 14:37:00 +0900

    프로그래머스 - 문자열 내 p와 y의 개수

---
+++ src/lv_1/Test9.java
@@ -0,0 +1,52 @@
+package lv_1;
+
+/**
+ * 문자열 내 p와 y의 개수
+ * 문제 설명
+ * 대문자와 소문자가 섞여있는 문자열 s가 주어집니다.
+ * s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
+ * 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+ * 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+ *
+ * 제한사항
+ * 문자열 s의 길이 : 50 이하의 자연수
+ * 문자열 s는 알파벳으로만 이루어져 있습니다.
+ *
+ * 입출력 예
+ * s	answer
+ * "pPoooyY"	true
+ * "Pyy"	false
+ */
+public class Test9 {
+	public static void main(String[] args) {
+		String s = "pPoooyY";
+		boolean solution = new Solution().solution(s);
+		System.out.println("solution = " + solution);
+	}
+
+	static class Solution {
+		public boolean solution(String s) {
+			boolean answer = true;
+
+			int pCnt = 0;
+			int yCnt = 0;
+
+			String[] split = s.toUpperCase().split("");
+			for (String str : split) {
+				if (str.equals("P")) { pCnt++; }
+				if (str.equals("Y")) { yCnt++; }
+			}
+
+			answer = pCnt == yCnt ? true : false;
+
+			return answer;
+		}
+	}
+}
+
+/*
+	boolean solution(String s) {
+	    s = s.toUpperCase();
+	    return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+	}
+ */
