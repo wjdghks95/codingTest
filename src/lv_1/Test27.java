356a244fb4f826c6d163171a6b6386c2c95a08b9
JEONGHWANLEE <wjdghks2799@naver.com>
2023-09-25 10:13:54 +0900

    프로그래머스 - 문자열 다루기 기본

---
+++ src/lv_1/Test27.java
@@ -0,0 +1,67 @@
+package lv_1;
+
+/**
+ * 문자열 다루기 기본
+ * 문제 설명
+ * 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+ * 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+ *
+ * 제한 사항
+ * s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+ * s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+ *
+ * 입출력 예
+ * s	return
+ * "a234"	false
+ * "1234"	true
+ */
+public class Test27 {
+	public static void main(String[] args) {
+		String s = "1234";
+		boolean solution = new Solution().solution(s);
+		System.out.println("solution = " + solution);
+	}
+
+	static class Solution {
+		public boolean solution(String s) {
+			boolean answer = false;
+
+			if (s.length() == 4 || s.length() == 6) {
+				char[] chars = s.toCharArray();
+				for (char c : chars) {
+					if (Character.getNumericValue(c) < 10) {
+						answer = true;
+					} else {
+						answer = false;
+						break;
+					}
+				}
+			}
+			
+			return answer;
+		}
+	}
+}
+
+/*
+	class Solution {
+	  public boolean solution(String s) {
+	      if(s.length() == 4 || s.length() == 6){
+	          try{
+	              int x = Integer.parseInt(s);
+	              return true;
+	          } catch(NumberFormatException e){
+	              return false;
+	          }
+	      }
+	      else return false;
+	  }
+	}
+
+	class Solution {
+	  public boolean solution(String s) {
+	        if (s.length() == 4 || s.length() == 6) return s.matches("(^[0-9]*$)");
+	        return false;
+	  }
+	}
+ */
